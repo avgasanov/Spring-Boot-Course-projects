0d869d90cf4a390fcfb179df3199e2b10541788d
AVGasanov <avgasanov@gmail.com>
2019-03-29 23:37:42 +0400

    Add solution for Spring Framework Test Assignment #1

---
+++ spring5-recipe-app/src/test/java/guru/springframework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,41 @@
+package guru.springframework.spring5recipeapp.controllers;
+
+import guru.springframework.spring5recipeapp.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class IndexControllerTest {
+
+    IndexController indexController;
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+        String actualString = indexController.getIndexPage(model);
+        assertEquals("index", actualString);
+
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
